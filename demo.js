8094c2d7981a17447d63de2d97380922251254de
Fernando González <rmfernando@outlook.com>
2019-10-09 16:02:03 -0600

    demo_html_utl file created, request url function test

--- demo.js
+++ demo.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var dt = require('./myfirstmodule.js');                 //Export the file 
 
+//create a server object:
 http.createServer(function(req, res){                   //Create the server 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently:" + "<br/>" + dt.myDateTime());      //Write to the page
-    res.end();
+    res.write("The date and time are currently:" + "<br/>" + dt.myDateTime()); //write a response to the client
+    res.end();                      //end the response
 }).listen(8080);                    //Expose 8080 port 
