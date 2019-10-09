ebe2f458560219225d00b4e894248834d6c969fd
Fernando González <rmfernando@outlook.com>
2019-10-09 15:46:21 -0600

    Module and demo file updated.

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var dt = require('./myfirstmodule.js');
+var dt = require('./myfirstmodule.js');                 //Export the file 
 
-http.createServer(function(req, res){
+http.createServer(function(req, res){                   //Create the server 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently:" + "<br/>" + dt.myDateTime());
+    res.write("The date and time are currently:" + "<br/>" + dt.myDateTime());      //Write to the page
     res.end();
-}).listen(8080);
+}).listen(8080);                    //Expose 8080 port 
--- myfirstmodule.js
+++ myfirstmodule.js
@@ -1,3 +1,3 @@
-exports.myDateTime = function (){
-    return Date();
+exports.myDateTime = function (){           //Export the function 
+    return Date();                          //Date function to get the time 
 };
